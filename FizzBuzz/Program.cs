55570f0db8f6344fddb3df77bb259c1d92f6cd05
Ruby Lavender <ruby.lavender@softwire.com>
2020-07-06 15:25:25 +0100

    commiting FizzBuzz

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            for (int number = 1; number < 101; number++)
+            {
+                string output = "";
+                if (number % 3 == 0)
+                {
+                    output += "Fizz";
+                }
+
+                if (number % 5 == 0)
+                {
+                    output += "Buzz";
+                }
+
+                if (output == "")
+                {
+                    Console.WriteLine(number);
+                }
+                else
+                {
+                    Console.WriteLine(output);
+                }
+            }
+        }
+    }
+}
